a2cbf6f5cf0100160ae2812a16f9f5d70193a3b5
Abi Liu <abiliu018@gmail.com>
2023-04-11 18:42:22 -0700

    setting up backend

---
+++ server/controllers/auth.js
@@ -0,0 +1,105 @@
+const passport = require("passport");
+const validator = require("validator");
+const User = require("../models/User");
+
+const CLIENT_URL = "http://localhost:5173/";
+
+exports.postLogin = (req, res, next) => {
+  const validationErrors = [];
+  if (!validator.isEmail(req.body.email))
+    validationErrors.push({ msg: "Please enter a valid email address." });
+  if (validator.isEmpty(req.body.password))
+    validationErrors.push({ msg: "Password cannot be blank." });
+
+  if (validationErrors.length) {
+    req.flash("errors", validationErrors);
+    return res.status(500).json({ message: validationErrors });
+  }
+  req.body.email = validator.normalizeEmail(req.body.email, {
+    gmail_remove_dots: false,
+  });
+
+  passport.authenticate("local", (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      req.flash("errors", info);
+      return res.json({ message: "No user failed" });
+    }
+    req.logIn(user, (err) => {
+      if (err) {
+        return next(err);
+      }
+      req.flash("success", { msg: "Success! You are logged in." });
+      res.status(200).json(user);
+    });
+  })(req, res, next);
+};
+
+exports.logout = (req, res) => {
+  req.logout(() => {
+    console.log("User has logged out.");
+  });
+  req.session.destroy((err) => {
+    if (err)
+      console.log("Error : Failed to destroy the session during logout.", err);
+    req.user = null;
+    res.redirect(CLIENT_URL);
+  });
+};
+
+exports.postSignup = (req, res, next) => {
+  const validationErrors = [];
+  if (!validator.isEmail(req.body.email))
+    validationErrors.push({ msg: "Please enter a valid email address." });
+  if (!validator.isLength(req.body.password, { min: 6 }))
+    validationErrors.push({
+      msg: "Password must be at least 6 characters long",
+    });
+  if (req.body.password !== req.body.confirmPassword)
+    validationErrors.push({ msg: "Passwords do not match" });
+
+  if (validationErrors.length) {
+    req.flash("errors", validationErrors);
+    return res.status(500).json(validationErrors);
+  }
+  req.body.email = validator.normalizeEmail(req.body.email, {
+    gmail_remove_dots: false,
+  });
+
+  const user = new User({
+    userName: req.body.userName,
+    email: req.body.email,
+    password: req.body.password,
+  });
+
+  User.findOne(
+    { $or: [{ email: req.body.email }, { userName: req.body.userName }] },
+    (err, existingUser) => {
+      if (err) {
+        return next(err);
+      }
+      if (existingUser) {
+        req.flash("errors", {
+          msg: "Account with that email address or username already exists.",
+        });
+        return res.status(500).json({
+          message:
+            "Account with that email address or username already exists.",
+        });
+      }
+      user.save((err) => {
+        if (err) {
+          return next(err);
+        }
+        req.logIn(user, (err) => {
+          if (err) {
+            return next(err);
+          }
+          res.status(200).json(user);
+        });
+      });
+    }
+  );
+};
---
+++ server/server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const passport = require("passport");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const connectDB = require("./config/database");
+const authRoutes = require("./routes/auth");
+
+require("dotenv").config({ path: "./config/.env" });
+
+// Passport config
+require("./config/passport")(passport);
+
+connectDB();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Sessions
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use("/auth", authRoutes);
+
+app.listen(process.env.PORT, () => {
+  console.log("Server is running, you better catch it!");
+});
