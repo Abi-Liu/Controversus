d9cf685f08f213451ef9e47db6504810eba6e074
seanmckee <seanmingmckee@gmail.com>
2023-04-11 20:09:07 -0700

    login page

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,15 +4,15 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div className="w-screen bg-red-400 p-6">
-      <ul className="flex">
-        <h1 className="text-slate-50 mr-10 text-6xl">C</h1>
-        <Link className="text-slate-50 mx-3 mt-3" to="/">
-          Home
-        </Link>
-        <Link className="text-slate-50 mx-3 mt-3" to="/register">
-          Register
-        </Link>
-      </ul>
+      <Link className="text-slate-50 mx-3" to="/">
+        Home
+      </Link>
+      <Link className="text-slate-50 mx-3" to="/login">
+        Login
+      </Link>
+      <Link className="text-slate-50 mx-3" to="/register">
+        Register
+      </Link>
     </div>
   );
 };
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+  const [formData, setFormData] = useState({});
+
+  function handleChange(event) {
+    const { value, id } = event.target;
+    setFormData((prev) => ({
+      ...prev,
+      [id]: value,
+    }));
+  }
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.post("http://localhost:8000/auth/signup", {
+        userName,
+        password,
+      });
+      alert("Registration Complete! Login Now.");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="text-center p-8 drop-shadow-lg">
+      <h1 className="text-3xl mb-2">Login</h1>
+      <form className="flex flex-col items-center" onSubmit={onSubmit}>
+        <input
+          type="text"
+          id="username"
+          value={formData.username}
+          onChange={handleChange}
+          placeholder="Username"
+          className="text-center rounded-full focus:outline-0 border-solid border-2 p-2 px-8 m-3"
+        />
+        <input
+          type="password"
+          id="password"
+          value={formData.password}
+          onChange={handleChange}
+          placeholder="Password"
+          className="text-center rounded-full focus:outline-0 border-solid border-2 p-2 px-8 m-3"
+        />
+        <button
+          className="bg-red-400 rounded-full p-2 px-10 text-slate-50 mt-3"
+          type="submit"
+        >
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
